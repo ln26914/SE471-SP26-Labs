a9f21cf408106aea5884ce26a8ee0d5e06df375a
andzamul <andrewzmueller19@gmail.com>
2026-02-16 20:45:56 -0800

    Added sorting algorithms, just saving progress.

--- Lab1/SRC-1/Main.java
+++ Lab1/SRC-1/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 class Product {
     // Initializing variables
     public static int global_ID = 0;
@@ -41,17 +45,75 @@ class Product {
 }
 
 class SortingProxy {
-    
+    public void log(ArrayList<Product> sortedList){
+    // BubbleSort will log as ID, name, price
+    // QuickSort will log as name, ID, price
+    }
 }
 
 class SortingUtility {
+    public ArrayList<Product> sort(ArrayList<Product> unsortedList, int sortingApproach){
+        
+        return unsortedList;
+    }
+    // BubbleSort 
+    private void bubbleSort(List<Product> list) {
+    int n = list.size();
+
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - 1 - i; j++)
+            if (list.get(j).getPrice() > list.get(j + 1).getPrice()) {
+                Product temp = list.get(j);
+                list.set(j, list.get(j + 1));
+                list.set(j + 1, temp);
+            }
+}
     
+    // QuickSort 
+    private void quickSort(List<Product> list, int low, int high) {
+    if (low >= high) return;
+
+    double pivot = list.get(high).getPrice();
+    int i = low;
+
+    for (int j = low; j < high; j++) {
+        if (list.get(j).getPrice() <= pivot) {
+            Product temp = list.get(i);
+            list.set(i, list.get(j));
+            list.set(j, temp);
+            i++;
+        }
+    }
+
+    Product temp = list.get(i);
+    list.set(i, list.get(high));
+    list.set(high, temp);
+
+    quickSort(list, low, i - 1);
+    quickSort(list, i + 1, high);
+}
+
 }
 
 public class Main
 {
 	public static void main(String[] args) {
-	    Product myNewProduct = new Product("Book", 10.99);
-		System.out.printf("Product name: %s\nPrice: %.2f\nID: %d\n", myNewProduct.getName(), myNewProduct.getPrice(), myNewProduct.getID());
-	}
+	    Product product1 = new Product("Book", 10.99);
+	    Product product2 = new Product("Button", 0.99);
+	    Product product3 = new Product("Bag", 22.99);
+	    Product product4 = new Product("Car", 2500.00);
+	    Product product5 = new Product("Fancy Ring", 150.00);
+	    ArrayList<Product> unsortedListOfProducts = new ArrayList<Product>();
+	    unsortedListOfProducts.add(product1);
+	    unsortedListOfProducts.add(product2);
+	    unsortedListOfProducts.add(product3);
+	    unsortedListOfProducts.add(product4);
+	    unsortedListOfProducts.add(product5);
+	    System.out.println("Step 1: Unsorted array of products:");
+	    System.out.printf("%-13s%-5s%-5s\n%s\n", "Name", "ID", "Price", "-------------------------");
+        for (int i = 0; i < unsortedListOfProducts.size(); i++) {
+            System.out.printf("%-13s%-5d%-5.2f\n", unsortedListOfProducts.get(i).name, unsortedListOfProducts.get(i).ID, unsortedListOfProducts.get(i).price);
+            
+            }	  
+        }
 }
